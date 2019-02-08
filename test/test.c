be08cacf6a3bdf94ec3ab1a27dca9b51578b76e4
Matheus Xavier <mxs@compuraven.com>
2019-02-07 23:38:29 -0200

    Now the test is working

--- test/test.c
+++ test/test.c
@@ -3,10 +3,27 @@
 #include <utfc.h>
 #include <memory.h>
 #include <stdio.h>
-#define DEBUG
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0')
+
 void main() {
   const char *helloascii = "Hello utf-8";
   utf8_char *encoded = EncodeAscii(helloascii, strlen(helloascii));
-
-  printf("#""BYTE_TO_BINARY_PATTERN""\n", BYTE_TO_BINARY_PATTERN());
+  for (int _ = 0; _ < strlen(helloascii); _++) {
+    printf("bytes of char %i", _);
+    printf(" > "BYTE_TO_BINARY_PATTERN"-"BYTE_TO_BINARY_PATTERN"-"BYTE_TO_BINARY_PATTERN"-"BYTE_TO_BINARY_PATTERN,
+           BYTE_TO_BINARY(encoded[_].bytes[0]),
+           BYTE_TO_BINARY(encoded[_].bytes[1]),
+           BYTE_TO_BINARY(encoded[_].bytes[2]),
+           BYTE_TO_BINARY(encoded[_].bytes[3]));
+    printf(" > %c \n", encoded[_].bytes[0]);
+  }
 }
