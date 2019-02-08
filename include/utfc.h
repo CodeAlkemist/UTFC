ba218d911d7427c29100d90ab3076675f579e3f6
Matheus Xavier <mxs@compuraven.com>
2019-02-07 22:23:08 -0200

    Update some files

--- include/utfc.h
+++ include/utfc.h
@@ -4,6 +4,18 @@
 #define UTFC_LIBRARY_H
 
 #include <stdlib.h>
+#ifdef DEBUG
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0')
+#endif
 /**
  *
  */
@@ -16,7 +28,7 @@ typedef struct utf8_codepoint__ {
 } utf8_char;
 
 /**
- *  Parse a raw arbitrary amount of bytes into a utf-8 string.
+ * Parse a raw arbitrary amount of bytes into a utf-8 string.
  * @param bytes
  * @param size
  * @return A utf8_string corresponding to the characters identified in the bytes passed
--- src/utfc.c
+++ src/utfc.c
@@ -13,19 +13,21 @@ utf8_char *ParseRawBytes(byte *bytes, size_t size) {
 }
 
 utf8_char *EncodeAscii(const char *string, size_t size) {
-  int mask = 0b10000000;
+  int mask = 0b01111111;
   byte bytes[size];
   utf8_char *utf8Char = malloc(sizeof(utf8Char)*size);
-  if (utf8Char == NULL) {
+  if (utf8Char==NULL) {
     errno = ENOMEM;
     return NULL;
   }
-
+  // Apply the bitmask to each char in the string
   for (int _ = 0; _ < size; _++) {
-    bytes[_] = (byte) (string[_] | mask);
+    bytes[_] = (byte) (string[_] & mask);
   }
 
   for (int _ = 0; _ < size; _++) {
-
+    utf8Char[_].bytes[0] = bytes[_];
   }
+
+  return utf8Char;
 }
